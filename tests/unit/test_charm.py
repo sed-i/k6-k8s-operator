0d1873bc966ce7013fb5856c8ffcd296d08c1afa
sed-i <82407168+sed-i@users.noreply.github.com>
2024-08-27 17:50:54 -0400

    Init commit

---
+++ tests/unit/test_charm.py
@@ -0,0 +1,24 @@
+# Copyright 2024 sed-i
+# See LICENSE file for licensing details.
+#
+# Learn more about testing at: https://juju.is/docs/sdk/testing
+
+import ops
+import ops.testing
+import pytest
+from charm import K6K8SCharm
+
+
+@pytest.fixture
+def harness():
+    harness = ops.testing.Harness(K6K8SCharm)
+    harness.begin()
+    yield harness
+    harness.cleanup()
+
+
+def test_pebble_ready(harness: ops.testing.Harness[K6K8SCharm]):
+    # Simulate the container coming up and emission of pebble-ready event
+    harness.container_pebble_ready("some-container")
+    # Ensure we set an ActiveStatus with no message
+    assert harness.model.unit.status == ops.ActiveStatus()
